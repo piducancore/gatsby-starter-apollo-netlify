7bd28c99927e19b0e558a74541ae3028717ee8d3
tetsuo shima <primaveraentalca@gmail.com>
2019-07-24 11:37:27 -0400

    use developMiddleware to proxy to our local server while developing

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,20 @@
+const proxy = require("http-proxy-middleware")
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
+  },
+  developMiddleware: app => {
+    app.use(
+      proxy("/.netlify/functions/", {
+        target: "http://localhost:9000/",
+        pathRewrite: {
+          "^/\\.netlify/functions": "",
+        },
+      })
+    )
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
