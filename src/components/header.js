1c3ba12074dc0fc34a7a7cf02e7f49df62887c30
tetsuo shima <primaveraentalca@gmail.com>
2019-07-26 04:22:44 -0400

    add links to icons in header

--- src/components/header.js
+++ src/components/header.js
@@ -16,22 +16,28 @@ const Header = ({ siteTitle }) => (
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <img
-        alt="Gatsby"
-        src="https://www.gatsbyjs.org/monogram.svg"
-        width="60"
-      />
-      <img
-        alt="Apollo"
-        src="https://rawgit.com/apollographql/apollo-client/master/docs/source/logo/square.png"
-        width="60"
-        style={{ marginLeft: "16px", marginRight: "16px" }}
-      />
-      <img
-        alt="Netlify"
-        src="https://www.netlify.com/img/press/logos/logomark.png"
-        width="60"
-      />
+      <a href="https://www.gatsbyjs.org">
+        <img
+          alt="Gatsby"
+          src="https://www.gatsbyjs.org/monogram.svg"
+          width="48"
+        />
+      </a>
+      <a href="https://www.apollographql.com/">
+        <img
+          alt="Apollo"
+          src="https://rawgit.com/apollographql/apollo-client/master/docs/source/logo/square.png"
+          width="48"
+          style={{ marginLeft: `16px`, marginRight: `16px` }}
+        />
+      </a>
+      <a href="https://www.netlify.com">
+        <img
+          alt="Netlify"
+          src="https://www.netlify.com/img/press/logos/logomark.png"
+          width="48"
+        />
+      </a>
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
