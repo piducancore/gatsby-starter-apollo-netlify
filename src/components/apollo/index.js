a0c29cb0784bb30256dcaa08936aefe2d2e5aa9f
tetsuo shima <primaveraentalca@gmail.com>
2019-08-04 16:59:56 -0400

    add apollo auth headers

--- src/components/apollo/index.js
+++ src/components/apollo/index.js
@@ -1,7 +1,19 @@
 import ApolloClient from "apollo-boost"
 import fetch from "isomorphic-fetch"
 
+const getToken = () => {
+  const token = localStorage.getItem("auth-token")
+  return token ? `Bearer ${token}` : ""
+}
+
 export const client = new ApolloClient({
   uri: "/.netlify/functions/graphql",
+  request: operation => {
+    operation.setContext({
+      headers: {
+        authorization: getToken(),
+      },
+    })
+  },
   fetch,
 })
