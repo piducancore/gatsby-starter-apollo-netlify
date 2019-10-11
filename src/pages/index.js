f807e77ed203df452558ff460dde58f8dae0dd14
tetsuo shima <primaveraentalca@gmail.com>
2019-10-11 20:16:04 -0300

    replace Query component with useQuery hook

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 import gql from "graphql-tag"
-import { Query } from "react-apollo"
+import { useQuery } from "@apollo/react-hooks"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
@@ -13,37 +13,34 @@ const APOLLO_QUERY = gql`
   }
 `
 
-const IndexPage = props => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p> and...</p>
-    <Query query={APOLLO_QUERY}>
-      {({ data, loading, error }) => {
-        if (loading) return <p>Loading...</p>
-        if (error) return <p>Error: {error.message}</p>
-        return <h1>{data.hello}</h1>
-      }}
-    </Query>
-    <p>Welcome.</p>
-    <p>
-      Start by checking <code>src/pages/index.js</code> to see an example query
-      using the
-      <code>Query</code> component from <code>react-apollo</code>, and{" "}
-      <code>functions/graphql/graphql.js</code> to start editing your Apollo
-      Server.
-    </p>
-    <p>
-      You can find Apollo Server's GraphQL Playground at{" "}
-      <code>{props.location.origin}/.netlify/functions/graphql</code>
-    </p>
-    {console.log(props)}
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, margin: `1.45rem auto` }}>
-      <Image />
-      <Link to="/page-2/">Go to page 2</Link>
-    </div>
-  </Layout>
-)
+const IndexPage = props => {
+  const { data } = useQuery(APOLLO_QUERY)
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p> and...</p>
+      <h1>{data && data.hello}</h1>
+      <p>Welcome.</p>
+      <p>
+        Start by checking <code>src/pages/index.js</code> to see an example
+        query using the
+        <code>Query</code> component from <code>react-apollo</code>, and{" "}
+        <code>functions/graphql/graphql.js</code> to start editing your Apollo
+        Server.
+      </p>
+      <p>
+        You can find Apollo Server's GraphQL Playground at{" "}
+        <code>{props.location.origin}/.netlify/functions/graphql</code>
+      </p>
+      {console.log(props)}
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, margin: `1.45rem auto` }}>
+        <Image />
+        <Link to="/page-2/">Go to page 2</Link>
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage
