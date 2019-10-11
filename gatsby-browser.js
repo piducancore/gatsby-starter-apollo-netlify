ff99fbd579060f36af48b00ab12140a744bdd021
tetsuo shima <primaveraentalca@gmail.com>
2019-10-11 20:15:44 -0300

    replace react-apollo with @apollo/react-common

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,6 @@
 import React from "react"
-import { ApolloProvider } from "react-apollo"
-import { client } from "./src/components/apollo"
+import { ApolloProvider } from "@apollo/react-common"
+import { client } from "./src/apollo/client"
 
 export const wrapRootElement = ({ element }) => (
   <ApolloProvider client={client}>{element}</ApolloProvider>
