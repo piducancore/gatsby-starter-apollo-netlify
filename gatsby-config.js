33325c89f6c13641172f9d7d27dd27f30aa3191c
tetsuo shima <primaveraentalca@gmail.com>
2020-06-10 20:40:12 -0400

    remove unused plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,15 +9,6 @@ module.exports = {
     `gatsby-theme-apollo`,
     `gatsby-plugin-react-helmet`,
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
